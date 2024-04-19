3ff37b6149e0a91fbecbff0bd3b5b0574e79661c
bjoarder7 <bjoarder87@gmail.com>
2024-04-19 11:20:24 +0600

    DOM Manipulation start

---
+++ ticket.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-buyticket").addEventListener('click', function(){
+    const foc = document.getElementById("functional-section").scrollIntoView();
+    console.log(foc);
+})
